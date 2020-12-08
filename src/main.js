d16b5a17b37595093a6350b373b85bc785fe3fad
he.qing <he.qing@vpclub.cn>
2020-12-08 11:13:51 +0800

    PostCSS 适配配置

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router/router'
 import './index.css';
 import { Field , Button,Toast } from 'vant';
 import "/@modules/vant/lib/index.css";
+import '@vant/touch-emulator';
 
 const app = createApp(App)
 app.use(router);
