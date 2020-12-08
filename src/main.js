3cbbb1409b4c147eb40b7f7a8e82cfb3ef03c9d6
he.qing <he.qing@vpclub.cn>
2020-12-08 17:21:38 +0800

    底部空白

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router'
 import './index.css';
-import { Field , Button,Toast } from 'vant';
+import { Field , Button,Toast,Image as VanImage  } from 'vant';
 import "/@modules/vant/lib/index.css";
 import '@vant/touch-emulator';
 import 'amfe-flexible/index.js'
 const app = createApp(App)
 app.use(router);
-app.use(Field).use(Button).use(Toast)
+app.use(Field).use(Button).use(Toast).use(VanImage)
 app.mount('#app');
 
--- src/view/login.vue
+++ src/view/login.vue
@@ -9,10 +9,16 @@
 <template>
   <div class="login" >
     <!-- 背景色块 -->
-    <div class="login-colorLump">111</div>
-    <van-field v-model="phone" type="number" label="手机号" />
-    <van-field v-model="password" type="password" label="密码" />
-    <van-button @click="login" type="primary" size="small" class="login-button">登录</van-button>
+    <div class="login-colorLump">
+    <!-- 头像 -->
+    <van-image class="login-headImg" round width="100px" height="100px" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+    </div>
+    <!-- 登录表单 -->
+    <div class="login-form">
+    <van-field v-model="phone" type="number" left-icon="wap-home-o" clearable placeholder="手机号"/>
+    <van-field v-model="password" type="password"  left-icon="lock"  placeholder="密码"/>
+    <van-button @click="login" type="primary" size="normal" class="login-button" block style="margin-top:25px">登录</van-button>
+    <van-button @click="login" type="primary" size="normal" class="login-button" block style="backgroundColor:#ccc">注册</van-button>
   </div>
 </template>
 
@@ -55,13 +61,27 @@ export default {
   .login{
     overflow: hidden;
     width: 100%;
+    height: 100%;
     .login-colorLump{
       height: 200px;
-      background-color: pink;
+      background-color:rgb(208, 193, 198);
+      position: relative;
+      .login-headImg{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,50%);
+      }
     }
-     .login-button{
-      margin-top: 20px;
-     }
+    .login-form{
+      margin-top: 70px;
+      padding: 30px;
+      .login-button{
+        margin-top: 10px;
+        border: none;
+      }
+    }
+     
   }
 
 </style>
